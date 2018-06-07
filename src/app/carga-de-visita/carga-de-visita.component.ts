e4b125c1baa28a5fd7ddaf66f9246a05391bc954
sergioZele <szelechowski@americavirtualsa.com>
2018-06-07 00:20:05 -0300

    se puede entrar a la mascota seleccionada

--- src/app/carga-de-visita/carga-de-visita.component.ts
+++ src/app/carga-de-visita/carga-de-visita.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-carga-de-visita',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CargaDeVisitaComponent implements OnInit {
 
-  constructor() { }
+  id : string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  this.route.params.subscribe(params => {
+     this.id = params['id'];
+   });
+
   }
 
 }
