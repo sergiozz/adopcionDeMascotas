75a98a2d280ff526697c4b41fecf5bcf2053d13d
sergioz <sergiozz123@gmail.com>
2018-07-03 00:55:53 -0300

    se conecta con el backend la parte del perfil y el historico

--- src/app/carga-de-visita/carga-de-visita.component.ts
+++ src/app/carga-de-visita/carga-de-visita.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Mascota } from '../models/mascota';
 import { MascotasService } from '../services/mascotas.service';
+import { Estudio } from '../models/estudio';
 
 @Component({
   selector: 'app-carga-de-visita',
@@ -13,13 +14,20 @@ export class CargaDeVisitaComponent implements OnInit {
 
   id : string;
   dateValue : string;
+  razaMascota : String;
   moneyValue : number;
   mascota : Mascota;
   pathAvatar: string = 'http://localhost/mascotas/Imagenes/';
+  estudiosHistorico: Estudio[];
+  showSpinnerPerfil: boolean;
+  showSpinnerHistorico: boolean;
 
   constructor(private route: ActivatedRoute, private mascotasService: MascotasService) { }
 
   ngOnInit() {
+  this.estudiosHistorico = new Array<Estudio>();
+  this.showSpinnerPerfil = true;
+  this.showSpinnerHistorico = true;
   this.route.params.subscribe(params => {
      this.id = params['id'];
      this.cargaDeDatosIniciales();
@@ -29,10 +37,21 @@ export class CargaDeVisitaComponent implements OnInit {
  cargaDeDatosIniciales(){
    this.mascotasService.getMascota(this.id).then(data => {
      this.mascota = data;
+     this.mascotasService.getRaza(this.mascota.razaId).then(data2 => {
+       console.log(data2.descripcion)
+        this.razaMascota = data2.descripcion;
+        this.showSpinnerPerfil = false;
+     });
      console.log(this.mascota);
      // this.isEnabled = true;
      // this.showSpinner = false;
    });
+
+   this.mascotasService.getEstudios(this.id).then(data => {
+     console.log(data);
+     this.estudiosHistorico = data;
+     this.showSpinnerHistorico = false;
+    });
   }
 
 }
--- src/app/services/mascotas.service.ts
+++ src/app/services/mascotas.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http , Headers } from '@angular/http';
 
 import { Mascota } from '../models/mascota';
+import { Estudio } from '../models/estudio';
 
 @Injectable()
 export class MascotasService {
@@ -37,6 +38,26 @@ export class MascotasService {
 
     getMascota(id:any): Promise<Mascota> {
         let recurso = this.path +"api/Mascotas/";
+
+        return this.http
+            .get(recurso, {params: {id: id}})
+            .toPromise()
+            .then(response => response.json() as any[])
+            .catch(this.handleError);
+    }
+
+    getRaza(id:any): Promise<any> {
+        let recurso = this.path +"api/Razas/";
+
+        return this.http
+            .get(recurso, {params: {id: id}})
+            .toPromise()
+            .then(response => response.json() as any[])
+            .catch(this.handleError);
+    }
+
+    getEstudios(id:any): Promise<Estudio[]> {
+        let recurso = this.path +"GetEstudioMascota";
 
         return this.http
             .get(recurso, {params: {id: id}})
