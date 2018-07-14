561207df133360ead6546bb09642b183a84fc124
Ricardo <estebaneze@gmail.com>
2018-07-14 16:58:37 -0300

    *Se agrega campo "fecha_vencimientoString" a visita con fecha formateada sin timepo

--- src/app/models/estudio.ts
+++ src/app/models/estudio.ts
@@ -6,7 +6,7 @@ export class Estudio {
   tipoEstudioId :string;
   fecha_realizacion :string;
   veterinarioId :string;
-  fecha_vencimiento :string;
+  fecha_vencimientoString :string;
   observaciones :string;
   tipoEstudio :TipoEstudio;
 }
